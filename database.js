f7d42c13535a319dc2f5f7560abe7f96369dfc16
gopinathboyanapally <gboyanapally@gmail.com>
2025-01-26 19:33:19 -0700

    pwd changed

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ import mysql from 'mysql2/promise';
 const connection = await mysql.createConnection({
     host: 'localhost',
     user: 'root', // You can change the username here as per you config
-    password: 'Lionelmess!10', // You can change the password here as per you config
+    password: 'password', // You can change the password here as per you config
     database: 'tododb'
 });
 
